c5f5e0489d5a8e8bad9d1f20fe017882e78e820a
neVer-777 <tbs.kleinknecht@gmail.com>
2025-06-15 16:23:50 +0200

    Delete serviceWorkerRegistration.js

--- serviceWorkerRegistration.js
+++
@@ -1,9 +0,0 @@
-
-// Simplified service worker registration for CRA
-export function register() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(() => console.log('Service Worker registered'))
-      .catch(err => console.error('Service Worker registration failed:', err));
-  }
-}
